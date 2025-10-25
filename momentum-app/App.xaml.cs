fb564b3c65e7acf984c71f8a3d9c2a075d612da2
luisboff <luisfernandoboff@gmail.com>
2025-10-25 15:01:33 -0300

    cleaning the space

--- momentum-app/App.xaml.cs
+++ momentum-app/App.xaml.cs
@@ -2,9 +2,25 @@
 {
     public partial class App : Application
     {
+        const int WindowWidth = 400;
+        const int WindowHeight = 750;
         public App()
         {
             InitializeComponent();
+
+            Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+            {
+#if WINDOWS
+
+                var mauiWindow = handler.VirtualView;
+                var nativeWindow = handler.PlatformView;
+                nativeWindow.Activate();
+                IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+                var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+                var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                appWindow.Resize(new Windows.Graphics.SizeInt32(WindowWidth, WindowHeight));
+#endif
+            });
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
