59587069b55633abf0e21365c6d3c7d30a013c5d
luisboff <luisfernandoboff@gmail.com>
2025-11-09 19:06:34 -0300

    creation of configs to pomodoro and some bugs

--- momentum-app/Pages/ConfigPage.xaml.cs
+++ momentum-app/Pages/ConfigPage.xaml.cs
@@ -2,8 +2,40 @@ namespace momentum_app.Pages;
 
 public partial class ConfigPage : ContentPage
 {
-	public ConfigPage()
-	{
-		InitializeComponent();
-	}
+    public ConfigPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        LoadSettings();
+    }
+
+    private void LoadSettings()
+    {
+        enFocusTime.Text = Preferences.Get("FocusDuration", 25).ToString();
+        enShortBreak.Text = Preferences.Get("ShortBreakDuration", 5).ToString();
+        enLongBreak.Text = Preferences.Get("LongBreakDuration", 15).ToString();
+        enCycles.Text = Preferences.Get("CyclesForLongBreak", 4).ToString();
+    }
+
+    private void bt_save_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Preferences.Set("FocusDuration", Convert.ToDouble(enFocusTime.Text));
+            Preferences.Set("ShortBreakDuration", Convert.ToDouble(enShortBreak.Text));
+            Preferences.Set("LongBreakDuration", Convert.ToDouble(enLongBreak.Text));
+            Preferences.Set("CyclesForLongBreak", Convert.ToDouble(enCycles.Text));
+
+            DisplayAlert("Success", "Settings saved.", "OK");
+            Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Error", "Please, insert valid numeric values.", "OK");
+        }
+    }
 }
